3089b25a263e25e898d7f9ed63e0d10c653a0dcc
Sunteresa7 <88226116+Sunteresa7@users.noreply.github.com>
2025-06-30 16:05:53 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "todo_db");
+$host = 'todo-db.c90koim8kxpr.ap-southeast-1.rds.amazonaws.com';
+$user = 'admin';
+$pass = 'your_secure_password123';
+$dbname = 'todo_db';
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+$conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
